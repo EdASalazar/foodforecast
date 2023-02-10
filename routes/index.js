38ec54c6ce0726aea6d7509c9d96c870defcc292
EdASalazar <edasalazar123@gmail.com>
2023-02-10 14:21:45 -0800

    Fixed the issue with the router and passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  // telling passport which passport strategy to use
+  'google', 
+  {
+    scope: ['profile', 'email'],
+    //optional
+    prompt: 'select_account',
+  }
+))
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    //change to what's best for your app
+    failureRedirect: '/',
+  }
+));
+
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    // change path for your landing page
+    res.redirect('/');
+  });
+});
+
+
 
 module.exports = router;
