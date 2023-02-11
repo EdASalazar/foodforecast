69c6bdd00683c90f1ee84fc6a5837156fea363c2
EdASalazar <edasalazar123@gmail.com>
2023-02-10 17:18:44 -0800

    Fist pass at the review Schema

--- models/review.js
+++ models/review.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-
+    blog: String,
+    rating: Number,
+    price: Number,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
 });
 
 
