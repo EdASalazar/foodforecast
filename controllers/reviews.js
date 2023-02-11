af4f7a2dd6f32f5f13b78d5df67d640951f2e11f
EdASalazar <edasalazar123@gmail.com>
2023-02-10 23:33:47 -0800

    Got the add review working for the most part just need to add the username to it. Oof long day.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,6 +4,19 @@ const User = require('../models/user');
 module.exports = {
     index,
     show,
+    new: newReview,
+    create,
+}
+
+function create(req, res) {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+      }
+    const review = new Review(req.body);
+    review.save(function(err) {
+        if (err) return res.redirect('reviews/new');
+        res.redirect('/reviews');
+    });
 }
 
 function index(req, res) {
@@ -18,4 +31,8 @@ function show(req, res) {
     Review.findById(req.params.id, function(err, review) {
         res.render('reviews/show', { title: 'Review Details', review });      
     });
+}
+
+function newReview(req, res) {
+    res.render('reviews/new', { title: 'Add Reviews',})
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -6,8 +6,10 @@ const reviewsCtrl = require('../controllers/reviews')
 //all routes start with reviews. 
 // GET /reviews 
 router.get('/', reviewsCtrl.index);
-// POST /reviews/new has to go before id
-
+// POST /reviews new
+router.post('/', reviewsCtrl.create);
+// GET /reviews/new has to go before id
+router.get('/new', reviewsCtrl.new);
 // GET /reviews/:id show
 router.get('/:id', reviewsCtrl.show);
 
