ba83d50c04299f18c9d438f7dd69cf30ce0f3611
EdASalazar <edasalazar123@gmail.com>
2023-02-11 10:14:45 -0800

    Added log in security to two routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const reviewsCtrl = require('../controllers/reviews')
-
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 //all routes start with reviews. 
 // GET /reviews 
 router.get('/', reviewsCtrl.index);
 // POST /reviews new
-router.post('/', reviewsCtrl.create);
-// GET /reviews/new has to go before id
-router.get('/new', reviewsCtrl.new);
+router.post('/', ensureLoggedIn, reviewsCtrl.create);
+// GET /reviews/new 
+router.get('/new', ensureLoggedIn, reviewsCtrl.new);
 // GET /reviews/:id show
 router.get('/:id', reviewsCtrl.show);
 
